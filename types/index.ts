623d292e2a0de8fab272cc23f4901df7193c473a
AlyonaCh <fresh1210@yandex.ru>
2026-02-08 12:48:24 +0500

    add base simple types

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+export type { Location } from './location'
+export type { Weather } from './weather'
+export type { City } from './city'
+export type { Country } from './country'
---
+++ types/weather.ts
@@ -0,0 +1,5 @@
+export interface Weather {
+  temp: number
+  description: string
+  icon: string
+}
